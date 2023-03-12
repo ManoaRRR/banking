39123df7d28ac52ff61a138373365ff6317ee1b8
ManoaRRR <hei.manoa.2@gmail.com>
2023-03-12 16:54:59 +0300

    update no permited exception files

--- src/main/java/com/projetTrans/prjetTrans/exceptions/OperationNonPermittedException.java
+++ src/main/java/com/projetTrans/prjetTrans/exceptions/OperationNonPermittedException.java
@@ -1,4 +1,13 @@
 package com.projetTrans.prjetTrans.exceptions;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public class OperationNonPermittedException extends RuntimeException{
+    private String errorMsg;
+    private String operationId;
+    private String source;
+    private String dependency;
 }
