c45e59b700f5a058a34e6e83c5a979013d953faf
ManoaRRR <hei.manoa.2@gmail.com>
2023-03-12 15:36:30 +0300

    add exception in validators

---
+++ src/main/java/com/projetTrans/prjetTrans/ObjectValidationException.java
@@ -0,0 +1,13 @@
+package com.projetTrans.prjetTrans;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import java.util.Set;
+@RequiredArgsConstructor
+public class ObjectValidationException extends RuntimeException{
+    @Getter
+    private final Set<String> validation;
+    @Getter
+    private final String violationSource;
+}
--- src/main/java/com/projetTrans/prjetTrans/ObjectValidators.java
+++ src/main/java/com/projetTrans/prjetTrans/ObjectValidators.java
@@ -1,5 +1,6 @@
 package com.projetTrans.prjetTrans;
 
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.stereotype.Service;
 
 import javax.validation.ConstraintViolation;
@@ -8,17 +9,19 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import java.util.Set;
 import java.util.stream.Collectors;
-
+//cette class est geenerique et elle permet de valider les objets ou leus valeurs sont entrées
 @Service
 public class ObjectValidators <T>{
     private  final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
     private  final Validator validator = validatorFactory.getValidator();
-    public void validate (T objectTovalidate){
-        Set<ConstraintViolation<T>> violations = validator.validate(objectTovalidate);
+    public void validate (T objectValidate){
+        Set<ConstraintViolation<T>> violations = validator.validate(objectValidate);
         if (!violations.isEmpty()){
             Set<String> errorMessage = violations.stream()
                     .map(ConstraintViolation::getMessage)
                     .collect(Collectors.toSet());
+            throw new ObjectValidationException(errorMessage, objectValidate.getClass().getName());
         }
+
     }
 }
